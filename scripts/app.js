c3aa426aa27d22385a045c18f928c46207bfbd44
Svetlana <124veg@gmail.com>
2022-12-29 17:33:52 +1100

    hid the modal and backdrop

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,11 @@ const backDropElement = document.getElementById('backdrop');
 const editPlayer1BtnElement = document.querySelector('#edit-plaer-1-btn');
 const editPlayer2BtnElement = document.querySelector('#edit-plaer-2-btn');
 
+const cancelConfigBtnElement = document.querySelector('#cancel-config-btn');
+
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
+
+cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
+backDropElement.addEventListener('click', closePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -2,3 +2,8 @@ function openPlayerConfig() {
   playerConfigOverlayElement.style.display = "block";
   backDropElement.style.display = "block";
 }
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backDropElement.style.display = "none";
+}
