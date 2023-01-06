cb17129950cb632068411acdea24483fdc99eda7
Svetlana <124veg@gmail.com>
2023-01-06 17:26:11 +1100

    fixed the problem with the error message not showing

--- scripts/config.js
+++ scripts/config.js
@@ -6,20 +6,23 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backDropElement.style.display = "none";
-  formElement.firstElementChild.remove('error');
-  errorsOutputElement.textContent = '';
+  formElement.firstElementChild.remove("error");
+  errorsOutputElement.textContent = "";
 }
-
 
 // спросить у Вовы. Почему не показывает сообщение внизу
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayername = formData.get("playername").trim();
+  const enteredPlayername = formData.get("playername");
 
-  if (enteredPlayername === '') {   
-    event.target.firstElementChild.classList.add('error');
-    errorsOutputElement.textContent = 'Please enter your name';
+
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter your name";
     return;
-  } // enteredPlayername === ''  
+  } // enteredPlayername === ''
+
+
+  
 }
